5aa8b7d3b67ce03fe96448d293a79830d64b8dad
TestUE4byNurlan <mukashevnurlan@gmail.com>
2020-06-24 18:00:44 +0600

    24-06-2020 _1

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,17 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 
 // firebase init goes here
-const config = {
-	apiKey: "",
-	authDomain: "",
-	databaseURL: "",
-	projectId: "",
-	storageBucket: "",
-	messagingSenderId: ""
-}
-firebase.initializeApp(config)
+var Config = {
+    apiKey: "AIzaSyCvYlsTL4J-MPEkqtsWnDAgDCWBBD_Lm6c",
+    authDomain: "testq-70155.firebaseapp.com",
+    databaseURL: "https://testq-70155.firebaseio.com",
+    projectId: "testq-70155",
+    storageBucket: "testq-70155.appspot.com",
+    messagingSenderId: "1022127045206",
+    appId: "1:1022127045206:web:e64ef3e9258365535fd9f7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(Config);
 
 // firebase utils
 const db = firebase.firestore()
